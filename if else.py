ca5e126d1e0d1f6778cf58c519752bb89b316623
Sahasav13 <sahaajaiv@gmail.com>
2025-03-25 21:49:17 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,12 @@
+temp = float(input("user : "))
+
+if temp < 10:
+    print("It's cold! Wear a warm coat and scarf.")
+
+elif temp <= 25:
+    print("It's mild! A light jacket will do.")
+
+else :
+    print("It's hot! T-shirt would be perfect.")
+
+    
