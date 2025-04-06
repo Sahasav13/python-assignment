db27437b8b6f81a94b10a7e0490f9970e2acd3b3
Sahasav13 <sahaajaiv@gmail.com>
2025-04-06 16:33:02 +0530

    Add files via upload

---
+++ recurrsion power.py
@@ -0,0 +1,14 @@
+
+def num(a,b):
+
+    if b==0:
+        return 1
+
+    else :
+        return a**(b)
+
+print(num(2,4))
+
+
+
+    
---
+++ recurrsion reverse.py
@@ -0,0 +1,10 @@
+
+def palindrome(x):
+
+    if x[::-1] == x:
+        return "True"
+
+    else :
+        return "False"
+
+print(palindrome("madam"))
